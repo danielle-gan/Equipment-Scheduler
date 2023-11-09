9e230fc9e3b7273cbd54c77c83b15feb0555df09
Danielle Gan <DGan@data-mail.com>
2023-11-09 13:01:14 -0500

    drag and drop functionality

--- script.js
+++ script.js
@@ -3,29 +3,77 @@ const button = document.getElementById('addBtn');
 
 const dragMe = document.getElementById('dragMe');
 
+const flex = document.getElementById('itemStage');
 
 
-// input.addEventListener('input', () => {
-//   if(input.value.length > 0) {
-//     button.disabled = false; 
-//   } else {
-//     button.disabled = true;
-//   }
-// });
+input.addEventListener('input', () => {
+  if(input.value.length > 0) {
+    button.disabled = false; 
+  } else {
+    button.disabled = true;
+  }
+});
 
+let count = 0;
 
 button.addEventListener('click', () => {
 
-    dragMe.style.display = 'flex';
+  const text = input.value;
 
-    const text = input.value;
+  // Create new div
+  const dragDiv = document.createElement('div');
+  dragDiv.classList.add('dragMe');
+  dragDiv.id = `drag${++count}`; // Increment id
 
-    const p = document.createElement('p');
-    p.textContent = text;
-    
-    dragMe.appendChild(p);
-    
-    input.value = '';
+  dragDiv.innerHTML += text;
+  
+  dragDiv.draggable = true;
+  dragDiv.ondragstart = dragStart;
+  // Append new div  
+  flex.appendChild(dragDiv);
 
-  });
+  input.value = '';
+});
 
+let draggedBlock;
+
+function dragStart(event) {
+  draggedBlock = event.target;
+  event.dataTransfer.setData('text/plain', null);
+}
+
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function removeDragOver(event) {
+  event.target.classList.remove('drag-over');
+}
+
+function drop(event) {
+  event.preventDefault();
+  event.target.classList.remove('drag-over');
+  if (event.target.classList.contains('dragInto')) {
+    event.target.appendChild(draggedBlock);
+  }
+  draggedBlock.classList.add('dragged');
+}
+
+
+
+// DELETE BUTTON
+const deleteBtn = document.getElementById('delete-btn');
+
+// Handle click
+deleteBtn.addEventListener('click', () => {
+
+  // Get dragDivs
+  const dragDivs = document.querySelectorAll('.dragMe');
+
+  // Loop through and remove
+  for(let i = 0; i < dragDivs.length; i++) {
+    dragDivs[i].remove();
+  }
+
+});
